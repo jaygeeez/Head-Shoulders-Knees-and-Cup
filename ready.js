46fca2718e9a25fb2543ac70f981aed73b588461
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-10-24 09:34:15 -0400

    Update ready.js

--- ready.js
+++ ready.js
@@ -3,7 +3,10 @@ var parts = ["head", "shoulders", "knees", "toes", "nose", "ears", "cup"];
 function roll() {
   var randomSeq = getRndInteger(1, 10);
   for (let i = 0; i < randomSeq; i++) {
-    newPart();
+    //make this better
+    setTimeout(() => {
+      newPart();
+    }, 1000);
   }
   document.getElementById("insertLevel").innerHTML += parts[parts.length - 1];
 }
