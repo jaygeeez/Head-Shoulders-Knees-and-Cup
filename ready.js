56980733385c40381a99c8f44f87eddeada64b4d
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-10-28 16:43:46 -0400

    main loop

--- ready.js
+++ ready.js
@@ -1,20 +1,18 @@
 var parts = ["head", "shoulders", "knees", "toes", "nose", "ears", "cup"];
 
 function roll() {
-  var randomSeq = getRndInteger(1, 10);
-  for (let i = 0; i < randomSeq; i++) {
-    //make this better,
-    setTimeout(() => {
-      newPart();
-    }, 1000);
-  }
-  document.getElementById("insertLevel").innerHTML += parts[parts.length - 1];
+  var wordRefresh = setInterval(() => {
+    newPart();
+  }, 1000);
 }
 
 function newPart() {
-  var randomPart = getRndInteger(0, parts.length - 1);
-  document.getElementById("insertLevel").innerHTML += parts[randomPart];
-  console.log(parts[randomPart]);
+  var randomPart = getRndInteger(0, parts.length);
+  if (document.getElementById("insertLevel").innerHTML != "cup") {
+    document.getElementById("insertLevel").innerHTML = parts[randomPart];
+  } else {
+    console.log("done");
+  }
 }
 
 function getRndInteger(min, max) {
