6c6547e3477c030521b2442d08db4bc2a3e25886
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-11-03 12:58:27 -0500

    timer

--- ready.js
+++ ready.js
@@ -4,18 +4,16 @@ function roll() {
   document.getElementById("insertPart").innerHTML = "";
   document.getElementById("retry").innerHTML = "";
   var wordRefresh = setInterval(() => {
-    newPart();
-  }, 1000);
-}
-
-function newPart() {
-  var randomPart = getRndInteger(0, parts.length);
-  if (document.getElementById("insertPart").innerHTML != "cup") {
-    document.getElementById("insertPart").innerHTML = parts[randomPart];
-  } else {
-    // Add a button to restart the game
-    document.getElementById("retry").innerHTML = "Click to try again.";
-  }
+    var randomPart = getRndInteger(0, parts.length);
+    if (document.getElementById("insertPart").innerHTML != "cup") {
+      document.getElementById("insertPart").innerHTML = parts[randomPart];
+    } else {
+      // Add a button to restart the game
+      document.getElementById("retry").innerHTML = "Click to try again.";
+      console.log("done");
+      clearInterval(wordRefresh);
+    }
+  }, getRndInteger(500, 2000));
 }
 
 function getRndInteger(min, max) {
